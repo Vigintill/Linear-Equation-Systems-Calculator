e8f09fa18306ed07ebb43d39f19c6f84f830cd74
Vigintil <vigintil5@gmail.com>
2024-10-11 00:22:10 +0400

    Метод печати матрицы, массив свободных членов и его заполнение

--- main.c
+++ main.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
-int main(void){
-    
-    
-    int x[3][3];
-    // printf("x1\t|x2\t|x3\t|b\t|");
-    for (unsigned short i = 0; i < 3; i++)
-    {
-        for (unsigned short j = 0; j < 3; j++)
+
+const int SIZE = 3;
+
+int printMatrix(int matrix[SIZE][SIZE], int sideMatrix[SIZE]){
+    printf("x1\t|x2\t|x3\t|b\t|\n");
+    for (unsigned short i = 0; i < SIZE; i++)
         {
-            printf("x[%d,%d]=",i,j);
+            for (unsigned short j = 0; j < SIZE; j++)
+            {
+                printf("%d\t|", matrix[i][j]);
+            }
+            printf("%d\t|", sideMatrix[i]);
+            printf("\n");
+        }
+
+    return 0;
+}
+
+int main(void){
+    int x[SIZE][SIZE];
+    int b[SIZE];
+    
+    for (unsigned short i = 0; i < SIZE; i++)
+    {
+        for (unsigned short j = 0; j < SIZE; j++)
+        {
+            printf("x[%d,%d] = ",i,j);
             scanf("%d",&x[i][j]);
         }
         printf("\n");
     }
+
+    for (unsigned short i = 0; i < SIZE; i++)
+    {
+        printf("b[%d] = ", i);
+        scanf("%d", &b[i]);
+    }
+    
+
+    printMatrix(x, b);
     
     return 0;
 }
+
