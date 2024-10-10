f0565c591bf743b979cde5770d42134afb639dbb
Vigintil <vigintil5@gmail.com>
2024-10-11 00:22:10 +0400

    Added matrix printing and filling methods

--- main.c
+++ main.c
@@ -1,18 +1,73 @@
 #include <stdio.h>
-int main(void){
-    
-    
-    int x[3][3];
-    // printf("x1\t|x2\t|x3\t|b\t|");
-    for (unsigned short i = 0; i < 3; i++)
-    {
-        for (unsigned short j = 0; j < 3; j++)
+
+int SIZE;
+
+int printMatrix(int matrix[SIZE][SIZE], int sideMatrix[SIZE]){
+    printf("|x1\t|x2\t|x3\t|b\t|\n");
+    for (unsigned short i = 0; i < SIZE; i++)
         {
-            printf("x[%d,%d]=",i,j);
-            scanf("%d",&x[i][j]);
+            for (unsigned short j = 0; j < SIZE; j++)
+            {
+                printf("|%d\t", matrix[i][j]);
+            }
+            printf("|%d\t|", sideMatrix[i]);
+            printf("\n");
+        }
+
+    return 0;
+}
+
+int scanfFillMatrix(int matrix[SIZE][SIZE], int sideMatrix[SIZE]){
+    printf("Заполните матрицу:\n");
+    for (unsigned short i = 0; i < SIZE; i++)
+    {
+        for (unsigned short j = 0; j < SIZE; j++)
+        {
+            printf("x[%d,%d] = ",i,j);
+            scanf("%d",&matrix[i][j]);
         }
         printf("\n");
     }
+
+    for (unsigned short i = 0; i < SIZE; i++)
+    {
+        printf("b[%d] = ", i);
+        scanf("%d", &sideMatrix[i]);
+    }
+
+    return 0;
+}
+
+int main(void){
+    printf("Укажите размер матрицы: ");
+    scanf("%d\n", &SIZE);
+
+    static const int _size;
+
+    int x[SIZE][SIZE];
+    int b[SIZE];
+    
+    scanfFillMatrix(x, b);
+    
+    printMatrix(x, b);
+
+    for (int step = 0; step < SIZE - 1; step++)
+    {        
+        for (int i = step + 1; i < SIZE; i++)
+        {
+            int q = x[i][step]/x[step][step];
+            for (int j = 0; j < SIZE; j++)
+            {
+                x[i][j] = x[i][j] - x[step][j] * q;
+            }
+
+            b[i] = b[i] - b[step] * q;
+        }
+        printf("\n");
+        printf("Шаг: %d\n", step + 1);
+        printMatrix(x, b);
+    }
+    
     
     return 0;
 }
